168e5cef8cffa9cd1b436f5c4a00150d4e92542b
Vagner Treguas <vagner.treguas@gmail.com>
2020-11-04 11:45:48 -0300

    add whatsapp

--- js/app.js
+++
@@ -1,8 +0,0 @@
-
-setTimeout(()=>{
-    document.getElementById('caixaTexto').style.visibility = 'visible';
-},5000);
-
-caixaTexto.onclick = ()=> {
-    document.getElementById('caixaTexto').style.visibility = 'hidden';
-};
